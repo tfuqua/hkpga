569fe6d787b7386a8bae14e4ef6b62f5d20f2426
Taylor Fuqua <tfuqua@gwu.edu>
2017-06-20 08:55:15 -0400

    Add route names to dynamic imports

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,49 @@
 import routeLoader from './util/routeLoader';
 
 //App Specific components
-const Home = routeLoader(() => import('./containers/Home'));
-const Register = routeLoader(() => import('./containers/Register'));
-const Forgot = routeLoader(() => import('./containers/Forgot'));
-const Page = routeLoader(() => import('./containers/Pages'));
-const Contact = routeLoader(() => import('./containers/Contact'));
-const AdminContact = routeLoader(() => import('./containers/Contact/admin'));
-const Tournaments = routeLoader(() => import('./containers/Tournaments'));
-const TournamentView = routeLoader(() => import('./containers/Tournament'));
-const Login = routeLoader(() => import('./containers/Login'));
-const Dashboard = routeLoader(() => import('./containers/Dashboard'));
-const AdminTournaments = routeLoader(() => import('./containers/Tournaments/tournaments'));
-const EditTournament = routeLoader(() => import('./containers/Tournaments/EditTournament'));
-const EditTournamentResults = routeLoader(() => import('./containers/Tournaments/EditResults'));
-const AdminPages = routeLoader(() => import('./containers/Pages/pages'));
-const EditPage = routeLoader(() => import('./containers/Pages/EditPage'));
-const AdminUsers = routeLoader(() => import('./containers/Users'));
-const EditUser = routeLoader(() => import('./containers/Users/EditUser'));
-const AdminArticles = routeLoader(() => import('./containers/Articles/articles'));
-const ArticleView = routeLoader(() => import('./containers/Articles/ArticleView'));
-const EditArticle = routeLoader(() => import('./containers/Articles/EditArticle'));
-const Pros = routeLoader(() => import('./containers/Pros'));
-const AdminPress = routeLoader(() => import('./containers/Press'));
-const EditPress = routeLoader(() => import('./containers/Press/EditPress'));
-const PressView = routeLoader(() => import('./containers/Press/PressView'));
-const ProView = routeLoader(() => import('./containers/Pros/ProView'));
-const Partners = routeLoader(() => import('./containers/Partners'));
-const Committee = routeLoader(() => import('./containers/Committee'));
-const Terms = routeLoader(() => import('./containers/Terms'));
-const Merit = routeLoader(() => import('./containers/Merit'));
-const AdminMerit = routeLoader(() => import('./containers/Merit/AdminMerit'));
-const MeritForm = routeLoader(() => import('./containers/Merit/MeritView'));
+const Home = routeLoader(() => import(/* webpackChunkName: "Home" */ './containers/Home'));
+const Register = routeLoader(() => import(/* webpackChunkName: "Register" */ './containers/Register'));
+const Forgot = routeLoader(() => import(/* webpackChunkName: "Forgot" */ './containers/Forgot'));
+const Page = routeLoader(() => import(/* webpackChunkName: "Pages" */ './containers/Pages'));
+const Contact = routeLoader(() => import(/* webpackChunkName: "Contact" */ './containers/Contact'));
+const AdminContact = routeLoader(() => import(/* webpackChunkName: "AdminContact" */ './containers/Contact/admin'));
+const Tournaments = routeLoader(() => import(/* webpackChunkName: "Tournaments" */ './containers/Tournaments'));
+const TournamentView = routeLoader(() => import(/* webpackChunkName: "Tournament" */ './containers/Tournament'));
+const Login = routeLoader(() => import(/* webpackChunkName: "Login" */ './containers/Login'));
+const Dashboard = routeLoader(() => import(/* webpackChunkName: "Dashboard" */ './containers/Dashboard'));
+const AdminTournaments = routeLoader(() =>
+  import(/* webpackChunkName: "AdminTournaments" */ './containers/Tournaments/tournaments')
+);
+const EditTournament = routeLoader(() =>
+  import(/* webpackChunkName: "EditTournament" */ './containers/Tournaments/EditTournament')
+);
+const EditTournamentResults = routeLoader(() =>
+  import(/* webpackChunkName: "EditResults" */ './containers/Tournaments/EditResults')
+);
+const AdminPages = routeLoader(() => import(/* webpackChunkName: "Pages" */ './containers/Pages/pages'));
+const EditPage = routeLoader(() => import(/* webpackChunkName: "EditPage" */ './containers/Pages/EditPage'));
+const AdminUsers = routeLoader(() => import(/* webpackChunkName: "AdminUsers" */ './containers/Users'));
+const EditUser = routeLoader(() => import(/* webpackChunkName: "EditUser" */ './containers/Users/EditUser'));
+const AdminArticles = routeLoader(() =>
+  import(/* webpackChunkName: "AdminArticles" */ './containers/Articles/articles')
+);
+const ArticleView = routeLoader(() =>
+  import(/* webpackChunkName: "ArticleView" */ './containers/Articles/ArticleView')
+);
+const EditArticle = routeLoader(() =>
+  import(/* webpackChunkName: "EditArticle" */ './containers/Articles/EditArticle')
+);
+const Pros = routeLoader(() => import(/* webpackChunkName: "Pros" */ './containers/Pros'));
+const AdminPress = routeLoader(() => import(/* webpackChunkName: "Press" */ './containers/Press'));
+const EditPress = routeLoader(() => import(/* webpackChunkName: "EditPress" */ './containers/Press/EditPress'));
+const PressView = routeLoader(() => import(/* webpackChunkName: "PressView" */ './containers/Press/PressView'));
+const ProView = routeLoader(() => import(/* webpackChunkName: "ProView" */ './containers/Pros/ProView'));
+const Partners = routeLoader(() => import(/* webpackChunkName: "Partners" */ './containers/Partners'));
+const Committee = routeLoader(() => import(/* webpackChunkName: "Committee" */ './containers/Committee'));
+const Terms = routeLoader(() => import(/* webpackChunkName: "Terms" */ './containers/Terms'));
+const Merit = routeLoader(() => import(/* webpackChunkName: "Merit" */ './containers/Merit'));
+const AdminMerit = routeLoader(() => import(/* webpackChunkName: "AdminMerit" */ './containers/Merit/AdminMerit'));
+const MeritForm = routeLoader(() => import(/* webpackChunkName: "MeritForm" */ './containers/Merit/MeritView'));
 
 export const routes = [
   {

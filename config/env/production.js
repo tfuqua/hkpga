c0d8e6d811a2f27572ea1e01d0c58dcb7ab8d03a
Taylor Fuqua <tfuqua@gwu.edu>
2017-09-19 08:53:38 -0400

    Updates to build files

--- config/env/production.js
+++ config/env/production.js
@@ -7,7 +7,9 @@ config.firebase.creds = {
   apiKey: 'AIzaSyCKVl4dyJM59an6sX5CcqVHSLgXfVvHTjM',
   authDomain: 'taylor-fuqua.firebaseapp.com',
   databaseURL: 'https://taylor-fuqua.firebaseio.com',
-  storageBucket: 'taylor-fuqua.appspot.com'
+  projectId: 'taylor-fuqua',
+  storageBucket: 'taylor-fuqua.appspot.com',
+  messagingSenderId: '442806122444'
 };
 
 module.exports = config;
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -31,40 +31,38 @@ class Login extends Component {
   }
 
   render() {
-    return this.props.isAuthenticated
-      ? <Redirect to="/admin" />
-      : <div className="container-fluid">
-          <div className="login-form">
-            <h2>
-              {translations[this.props.lang].LOGIN}
-            </h2>
-            <form onSubmit={this.handleSubmit} className="form">
-              <div className="form-group">
-                <TextField
-                  showError={this.state.showErrors}
-                  text={this.state.username}
-                  onFieldChanged={this.handleFieldChange('username')}
-                  placeholder="Email"
-                />
-              </div>
-              <div className="form-group border-less">
-                <TextField
-                  showError={this.state.showErrors}
-                  text={this.state.password}
-                  onFieldChanged={this.handleFieldChange('password')}
-                  type="password"
-                  placeholder="Password"
-                />
-              </div>
-              <input type="submit" value={translations[this.props.lang].LOGIN} className="btn btn-success btn-block" />
-            </form>
-          </div>
-          <div className="text-center">
-            <Link to="/forgot">
-              {translations[this.props.lang].RESETPASSWORD}
-            </Link>
-          </div>
-        </div>;
+    return this.props.isAuthenticated ? (
+      <Redirect to="/admin" />
+    ) : (
+      <div className="container-fluid">
+        <div className="login-form">
+          <h2>{translations[this.props.lang].LOGIN}</h2>
+          <form onSubmit={this.handleSubmit} className="form">
+            <div className="form-group">
+              <TextField
+                showError={this.state.showErrors}
+                text={this.state.username}
+                onFieldChanged={this.handleFieldChange('username')}
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group border-less">
+              <TextField
+                showError={this.state.showErrors}
+                text={this.state.password}
+                onFieldChanged={this.handleFieldChange('password')}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <input type="submit" value={translations[this.props.lang].LOGIN} className="btn btn-success btn-block" />
+          </form>
+        </div>
+        <div className="text-center">
+          <Link to="/forgot">{translations[this.props.lang].RESETPASSWORD}</Link>
+        </div>
+      </div>
+    );
   }
 }
 
